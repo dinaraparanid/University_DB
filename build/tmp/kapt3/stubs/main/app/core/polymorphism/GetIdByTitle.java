6c7ebf469b3d287e44d9a6b15bc8825854ad834b
Paranid5 <dinaraparanid@gmail.com>
2021-04-25 18:13:55 +0500

    super polymorphism + remove

--- build/tmp/kapt3/stubs/main/app/core/polymorphism/GetIdByTitle.java
+++
@@ -1,7 +0,0 @@
-package app.core.polymorphism;
-
-import java.lang.System;
-
-@kotlin.Metadata(mv = {1, 4, 2}, bv = {1, 0, 3}, k = 1, xi = 2, d1 = {"\u0000\n\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\b`\u0018\u00002\u00020\u0001\u00a8\u0006\u0002"}, d2 = {"Lapp/core/polymorphism/GetIdByTitle;", "", "Students"})
-public abstract interface GetIdByTitle {
-}
